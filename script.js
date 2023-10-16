491a81a2a50f5c340e85c2e1a4d0598e3c61eeaf
wond-kat <146868277+wond-kat@users.noreply.github.com>
2023-10-16 15:47:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-<!-- Reparse HTML for imported Code -->
-<script>
-  // if you use a different value for the ID of your embed
-  // then change it below - I used "svg-icon"
-  var embedId = "svg-icon";
-  var target = document.getElementById(embedId);
-  if (target != null) {
-    var targetText = target.innerHTML;
-    function decodeField(target) {
-      var dText = he.decode(target);
-      document.getElementById(embedId).innerHTML = dText;
-    }
-    // run the function to decode the target field content
-    decodeField(targetText);
-  }
-</script>
-    
-<!-- decode CMS collection item embed -->
-<script type="text/javascript">
-  $(document).ready(function () {
-    // targeting class of .dental-icon
-    $(".dental-icon").each(function () {
-      $(this).hide(); // hide so code is not displayed (better to hide in CSS)
-      $(this).html(he.decode($(this).html())); // decode the text
-      $(this).show(); // show the decoded element
-    });
-  });
-</script>
-
 <!-- Copywrite Current Year -->
 <script>
   // when the DOM is ready
@@ -51,4 +22,3 @@
     });
   });
 </script>
-
